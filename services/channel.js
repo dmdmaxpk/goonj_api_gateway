5de42ea000abb0a9a6231144736bbf07c8eb0e29
Usama <musamadev@gmail.com>
2018-08-29 20:23:51 +0500

    search replaced from name to slug

--- services/channel.js
+++ services/channel.js
@@ -3,19 +3,17 @@ exports.getChannel = async (req, res) => {
     const db = req.app.locals.db;
     const collection = db.collection('channels');
 
-	const { _id, category, name, slug } = req.query;
+	const { _id, slug } = req.query;
 	const query = {};
 
 	if (_id) query._id = _id;
-	if (category) query.category = category;
-	if (name) query.name = name.replace(/-/g, ' ');	// Replacing the '-' in the channel name written in url for name searching.
 	if (slug) query.slug = slug;
 	query.active = true;	// Only returning the active channels
 	// console.log("Channel Query:", query);
 
 	let result;
 
-	if (_id || name || slug) {		// If _id then findOne
+	if (_id || slug) {		// If _id then findOne
 		result = await collection.findOne(query);
 		if (result == null) result= [];		// If no result is found then return empty array
 	}
