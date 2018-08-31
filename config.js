4233b35b5af1c3d461ce4ceeb0f3b414f413c4ca
Usama <musamadev@gmail.com>
2018-09-01 04:27:12 +0500

    DB name changed from mongoose to telenor

--- config.js
+++ config.js
@@ -3,18 +3,18 @@ const env = process.env.NODE_ENV || 'development';
 let config = {
     development: {
         port: '3000',
-        mongoDbUrl: 'mongodb://localhost:27017/mongoose',
-        dbName: 'mongoose'
+        mongoDbUrl: 'mongodb://localhost:27017/telenor',
+        dbName: 'telenor'
     },
     staging: {
         port: '3000',
-        mongoDbUrl: 'mongodb://localhost:27017/mongoose',
-        dbName: 'mongoose'
+        mongoDbUrl: 'mongodb://localhost:27017/telenor',
+        dbName: 'telenor'
     },
     production: {
         port: '3000',
-        mongoDbUrl: 'mongodb://RootAdmin:password@mongo:27017/mongoose?authSource=admin&readPreference=secondaryPreferred',  // @mongo is the name of the serview i.e mongo // TODO: Setting it to Env Variable
-        dbName: 'mongoose'
+        mongoDbUrl: 'mongodb://RootAdmin:password@mongo:27017/telenor?authSource=admin&readPreference=secondaryPreferred',  // @mongo is the name of the serview i.e mongo // TODO: Setting it to Env Variable
+        dbName: 'telenor'
     }
 };
 
