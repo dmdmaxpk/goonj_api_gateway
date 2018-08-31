3cd3a662153114fd047e8d6749eb94169e361d0e
Usama <musamadev@gmail.com>
2018-09-01 04:27:54 +0500

    is_host and is_guest check removed

--- services/anchor.js
+++ services/anchor.js
@@ -3,12 +3,10 @@ exports.getAnchor = async (req, res) => {
     const db = req.app.locals.db;
     const collection = db.collection('anchors');
 
-	const { _id, is_host, is_guest, weightage } = req.query;
+	const { _id, weightage } = req.query;
 	const query = {};
 
 	if (_id) query._id = _id;
-	if (is_host) query.is_host = JSON.parse(is_host);
-	if (is_guest) query.is_guest = JSON.parse(is_guest);
 	if (weightage) query.weightage = Number(weightage);
 	// console.log("Anchors Query:", query);
 
