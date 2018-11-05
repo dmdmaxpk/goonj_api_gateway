10df7ada7461ac6af30766b3c2a5f87aec167963
Usama <musamadev@gmail.com>
2018-11-06 03:26:09 +0500

    config updated

--- config.js
+++ config.js
@@ -3,7 +3,9 @@ const env = process.env.NODE_ENV || 'development';
 let config = {
     development: {
         port: '3000',
-        mongoDbUrl: 'mongodb://localhost:27017/telenor',
+        mongoDbUrl: 'mongodb://RootAdmin:password@10.0.1.90:27017/telenor?authSource=admin&readPreference=secondaryPreferred', 
+        // mongoDbUrl: 'mongodb://RootAdmin:password@10.3.7.101:27017/telenor?authSource=admin&readPreference=secondaryPreferred',
+        // mongoDbUrl: 'mongodb://localhost:27017/mongoose',
         dbName: 'telenor'
     },
     staging: {
