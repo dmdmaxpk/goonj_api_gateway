f9e08fa3dcc8e6a00b3ee900770d4d753dfb985b
Usama <musamadev@gmail.com>
2018-12-18 14:33:44 +0500

    aplus for test apk

--- services/config.js
+++ services/config.js
@@ -1,8 +1,7 @@
 exports.settings = async (req, res) => {
     let data = {
         blabla: 'test2',
-        live_url: 'http://live.pockettv.com.pk:1936/live',
-        vod_url: 'http://live.pockettv.com.pk:555/curation',
+        live_url: 'http://streampk.goonj.pk/aplus.m3u8',
         api_url: 'http://192.168.10.121:3002',
         cdn_static: 'http://cloudfront.com',
         terms_url: 'https://api.goonj.pk/',
