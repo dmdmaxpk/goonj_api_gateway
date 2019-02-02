4e29181071890651c1378ea61ba57d0372ba354d
Usama <musamadev@gmail.com>
2019-02-03 02:56:24 +0500

    dynamic query for customized feed

--- services/video.js
+++ services/video.js
@@ -3,32 +3,78 @@ exports.getVideo = async (req, res) => {
     const db = req.app.locals.db;
     const collection = db.collection('videos');
 
-	const { _id, title, category, sub_category, added_dtm, feed, anchor, guests, topics, pinned, skip, limit, source } = req.query;
-	const query = {};
+	const { _id, category, feed, anchor, guests, topics, pinned, skip, limit, source, program } = req.query;
+	let query = {}, feedParams = {};
 
-	if (_id) query._id = _id;
-	if (title) query.title = title;
+	if (_id) 	  query._id = _id;
 	if (category) query.category = category;
-	if (sub_category) query.sub_category = sub_category;
-	if (anchor) query.$or = anchor.split(',').map( el => ({anchor: el}) );	// OR query for  Anchor
-	if (guests) query.guests = { $in: guests.split(',') };
-	if (topics) query.topics = { $in: topics.split(',') };
-	if (added_dtm) query.added_dtm = added_dtm;
-	if (feed) query.feed = feed;
-	// if (source) query.$or = source.split(',').map( el => ({source: el}) );	// OR query for  Source. WILL HAVE TO PUSH INTO ARRAY FOR OR
-	if (source) query.source = source;
-	if (pinned) query.pinned = JSON.parse(pinned);		// Conversion of string to Boolean
+	if (source)   query.source = source;
+	if (pinned)   query.pinned = JSON.parse(pinned);		// Conversion of string to Boolean
 	
-	// console.log("Video Query:", query);
+	// Params for customized feed
+	if (feed) 	 query.feed = feed;
+	if (anchor)  feedParams.anchor = anchor.split(',').map( el => ({anchor: el}) );	// OR query for  Anchor
+	if (program) feedParams.program = program.split(',').map( el => ({program: el}) );	// OR query for Program
+	if (topics)  feedParams.topics = { $in: topics.split(',') };
+	if (guests)  feedParams.guests = { $in: guests.split(',') };
+	
 	query.active = true;	// Get only active videos
+	// console.log("Video Query:", query);
 	let result;
 
 	if (_id) {		// If _id then findOne
+
 		result = await collection.findOne(query);
 		if (result == null) result= [];		// If no result is found then return empty array
 	}
+	else if (feed=='myfeed') {
+
+		let feedQuery = [];
+		if (program) feedQuery.push({ '$or': feedParams.program });
+		if (anchor)  feedQuery.push({ '$or': feedParams.anchor });
+		if (topics)  feedQuery.push({ '$or': [{ topics: feedParams.topics }] });
+		if (guests)  feedQuery.push({ '$or': [{ guests: feedParams.guests }] });
+		
+		// feedQuery sample output:
+		// console.dir(feedQuery, {depth: null});
+		// [ { '$or': [ { program: 'Behind The Wicket' } ] },
+		// { '$or': [ { anchor: 'Arifa Noor' }, { anchor: 'Abdul Moiz Jaferi' } ] },
+		// { '$or': [ { topics: { '$in': [ 'PTI', 'Asif Zardari' ] } } ] } ]
+		
+		result = await collection.find( { 
+			$and: [
+				// {$or: [ 
+				// 	{ '$or': query.program }, 	// Full form: { '$or': [ { program: 'NewsEye' }, { program: 'To The Point' } ] }
+				// 	{ '$or': query.anchor }, 	// Full form: { '$or': [ { anchor: 'Iftikhar Shirazi' }, { anchor: 'Arifa Noor' } ] }
+
+				// For topics 2 ways
+				// 	// { '$or': [ { topics: {$in: ['Social Media']} }, { topics: {$in: ['Lahore']} }] },	// Query rps: 59, 120, 120, 121
+				// 	// { '$or': [ { topics: {$in: ['Social Media', 'Rights of Women']} }] } 	// Query rps: 60, 129, 99, 118
+				// 	// { '$or': [ { topics: query.topics }] }
+
+				// Or all 3 can be combined in one OR
+				// {$or: [ { program: 'NewsEye' }, { anchor: 'Faisal Ilyas' }, { topics: {$in: ['Social Media', 'PTI']} } ]},
+				// ]},
+				
+				{ $or: feedQuery },
+				{ active: true }
+			]
+		} ).project({ 'active' : 0, 'transcoding_status' : 0, 'last_modified' : 0, '__v': 0 })
+		   .sort({ added_dtm:-1 })
+		   .skip( Number(skip) || 0 )
+		   .limit( Number(limit) || 16 )
+		   .toArray();		// Sorting by added_dtm, default for skip and limit is 0 and 16 respectively
+		
+		
+	}
 	else {
-		result = await collection.find(query).project({ 'active' : 0, 'transcoding_status' : 0, 'last_modified' : 0, '__v': 0 }).sort({ added_dtm:-1 }).skip( Number(skip) || 0 ).limit( Number(limit) || 16 ).toArray();		// Sorting by added_dtm, default for skip and limit is 0 and 16 respectively
+		result = await collection.find(query)
+								 .project({ 'active' : 0, 'transcoding_status' : 0, 'last_modified' : 0, '__v': 0 })
+								 .sort({ added_dtm: -1 })
+								 .skip( Number(skip) || 0 )
+								 .limit( Number(limit) || 16 )
+								 .toArray();		// Sorting by added_dtm, default for skip and limit is 0 and 16 respectively
+
 		// OR
 		// let options = {
 		// 	limit: Number(limit) || 16,
