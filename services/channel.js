dbf895a720c5a3d2780ef56ba0a5adba9c1b33f2
Usama <musamadev@gmail.com>
2019-05-23 15:25:58 +0500

    channel refactored

--- services/channel.js
+++ services/channel.js
@@ -9,7 +9,6 @@ exports.getChannel = async (req, res) => {
 	if (_id) query._id = _id;
 	if (slug) query.slug = slug;
 	query.active = true;	// Only returning the active channels
-	// console.log("Channel Query:", query);
 
 	let result;
 
@@ -18,9 +17,11 @@ exports.getChannel = async (req, res) => {
 		if (result == null) result= [];		// If no result is found then return empty array
 	}
 	else {
-		result = await collection.find(query)
-								 .project({ 'active' : 0, 'added_dtm' : 0, 'description' : 0, '__v': 0, 'country': 0, 'seq': 0, 'logo': 0, 'category': 0 })
-								 .sort({seq:1}).toArray();
+		result = await collection
+			.find(query)
+			.project({ 'active': 0, 'added_dtm': 0, 'description': 0, '__v': 0, 'country': 0, 'seq': 0, 'logo': 0, 'category': 0 })
+			.sort({ seq:1 })
+			.toArray();
 	}
 	res.send(result);
 };
