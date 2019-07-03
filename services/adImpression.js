f4a27ce4be62f316cb99576fdfeeea0364d63a42
connectsocial454 <tbk454@gmail.com>
2019-07-03 16:54:52 +0500

    Private IP

--- services/adImpression.js
+++ services/adImpression.js
@@ -2,7 +2,7 @@ const Axios = require('axios');
 exports.postVodAds = (req, res) => {
     let {view_date, ad_tag_source, source, source_id} = req.body;
 
-    Axios.post(`http://18.196.7.106:3007/vodAd?view_date=${view_date}&ad_tag_source=${ad_tag_source}&source=${source}&source_id=${source_id}`)
+    Axios.post(`http://10.0.1.70:3007/vodAd?view_date=${view_date}&ad_tag_source=${ad_tag_source}&source=${source}&source_id=${source_id}`)
     .then(data => {
         res.send("Posted");
         console.log("VOD Ad Impression Recorded")})
@@ -11,7 +11,7 @@ exports.postVodAds = (req, res) => {
 exports.postLiveAds = (req, res) => {
     let {view_date, ad_tag_source, source, source_id} = req.body;
 
-    Axios.post(`http://18.196.7.106:3007/liveAd?view_date=${view_date}&ad_tag_source=${ad_tag_source}&source=${source}&source_id=${source_id}`)
+    Axios.post(`http://10.0.1.70:3007/liveAd?view_date=${view_date}&ad_tag_source=${ad_tag_source}&source=${source}&source_id=${source_id}`)
     .then(data => {
         res.send("Posted");
         console.log("Live Ad Impression Recorded")})
