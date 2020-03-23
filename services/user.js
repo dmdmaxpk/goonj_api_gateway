f344b2d27e748287fda7cfbdadf6d354b3397792
Farhan Ali <farhan.ali@dmdmax.com>
2020-03-23 14:52:35 +0500

    Paywall requests/responses are being logged now

--- services/user.js
+++ services/user.js
@@ -6,61 +6,88 @@ const config = require('../config');
 
 // Send OTP
 exports.sendOtp = async (req, res) => {
-
 	const post = req.body;
-
+	
+	await sendReqBody(post, 'sendOtp');
 	let { data } = await axios.post(`${config.billingService}/user/sendOtp`, post);
+	await sendResBody(data, 'sendOtp');
+	
 	res.send(data);
 };
 
 // Validate OTP
 exports.validateOtp = async (req, res) => {
-
 	const { msisdn, otp, source  } = req.query;
-
+	
+	await sendReqBody({msisdn:msisdn, otp:otp, source:source}, 'validateOtp');
 	let { data } = await axios.get(`${config.billingService}/user/validateOtp?msisdn=${msisdn}&otp=${otp}&source=${source}`);
+	await sendResBody(data, 'validateOtp');
+	
 	res.send(data);
 };
 
 // Subscription
 exports.subscribe = async (req, res) => {
-
 	const post = req.body;
 
+	await sendReqBody(post, 'subscribe');
 	let { data } = await axios.post(`${config.billingService}/subscription/subscribeUser`, post);
+	await sendResBody(data, 'subscribe');
+
 	res.send(data);
 };
 
 // Unsubscription
 exports.unSubscribe = async (req, res) => {
-
 	const { key } = req.query;
 
+	await sendReqBody({key:key}, 'unSubscribe');
 	let { data } = await axios.get(`${config.billingService}/subscription/unsubscribe?key=${key}`);
+	await sendResBody(data, 'unSubscribe');
+
 	res.send(data);
 };
 
 // Unsubscription without encryption
 exports.unSub = async (req, res) => {
-
     const { key } = req.query;
-
+	
+	await sendReqBody({key:key}, 'unSub');
     let { data } = await axios.get(`${config.billingService}/subscription/unsub?key=${key}`);
-    res.send(data);
+	await sendResBody(data, 'unSub');
+
+	res.send(data);
 };
 
 // Subscription Status
 exports.subscriptionStatus = async (req, res) => {
-
 	const { msisdn } = req.query;
 
+	await sendReqBody({msisdn:msisdn}, 'subscriptionStatus');
 	let { data } = await axios.get(`${config.billingService}/subscription/checkStatus?msisdn=${msisdn}`);
+	await sendResBody(data, 'subscriptionStatus');
+
 	res.send(data);
 };
 
 // Packages details
 exports.packages = async (req, res) => {
 
+	await sendReqBody({}, 'packages');
 	let { data } = await axios.get(`${config.billingService}/user/packages`);
+	await sendResBody(data, 'packages');
 	res.send(data);
 };
+
+
+sendReqBody = async(body, method) => {
+	const reqBody = body;
+	reqBody.method = method
+	await axios.post(`${config.paywallLoggingService}/logger/logreq`, reqBody);
+}
+
+sendResBody = async(body, method) => {
+	const resBody = body;
+	resBody.method = method
+	await axios.post(`${config.paywallLoggingService}/logger/logres`, resBody);
+}
