0dd19a50f48c5790f33f79613e8b8c78d92b7071
Hamza <hamzashujaat218@gmail.com>
2020-06-20 02:38:32 +0500

    .

--- services/search.js
+++ services/search.js
@@ -9,13 +9,18 @@ exports.getSearch = async (req, res) => {
 	if (term) query.term = term;
 
 	let result;
-		
+	let andArr = [];
+	andArr.push({ active: true }) // Videos should be active
+	andArr.push({ $text: { $search: `\"${query.term}\"` }}) // Text search on Phrase. More info: https://docs.mongodb.com/manual/reference/operator/query/text/
+	if (is_premium) {
+		andArr.push({ active: true })
+	} else {
+		andArr.push({ active:{ $ne: true} })
+	}
+	
 	result = await collection
 		.find({ 
-			$and: [												// AND condition
-				{ active: true }, 								// Videos should be active
-				{ $text: { $search: `\"${query.term}\"` } } 	// Text search on Phrase. More info: https://docs.mongodb.com/manual/reference/operator/query/text/
-			] 
+			$and:  andArr	 
 		})
 		.project({ 'active' : 0, 'transcoding_status' : 0, 'last_modified' : 0, '__v': 0 })
 		.sort({ publish_dtm: -1 })			// Sort by desc publish_dtm
