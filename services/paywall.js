2c0d93e9e35f5508de5269c03d3e1c2387ec2f2e
Muhammad <muhammad.azam@dmdmax.com>
2020-11-24 20:20:56 +0500

    Api route for revenue stats

--- services/paywall.js
+++ services/paywall.js
@@ -364,6 +364,11 @@ exports.billing_stats = async (req,res) => {
 	res.send(data);
 }
 
+exports.revenue_stats = async (req,res) => {
+	let { data } = await axios.get(`${config.paymentService}/report/rev-statistics`);
+	res.send(data);
+}
+
 exports.pageView = async (req, res) => {
 	console.log("[pageview]");
 	const transaction_id = getTransactinId();
