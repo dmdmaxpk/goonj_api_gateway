c8a25940dd69c022c6e523eea476f3b0393e56fa
Farhan Ali <farhan.ali@dmdmax.com>
2021-08-23 13:52:26 +0500

    cors enables for staging

--- app.js
+++ app.js
@@ -13,12 +13,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/', router);
 
-app.use(cors())
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
+app.use(cors({
+    origin: '*'
+}));
 
 let db;
 
