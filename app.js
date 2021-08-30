f77e48b6a9f8ba22f34260b9ef8f03f16df589ee
Farhan Ali <farhan.ali@dmdmax.com>
2021-08-30 11:49:41 +0500

    cors commented

--- app.js
+++ app.js
@@ -5,17 +5,17 @@ const router = require('./router');
 const config = require('./config');
 const swStats = require('swagger-stats');
 
-const cors = require('cors');
+//const cors = require('cors');
 
 const app = express();
 app.use(swStats.getMiddleware({}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(cors({
-    origin: '*',
-    methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH', 'OPTIONS']
-}));
+// app.use(cors({
+//     origin: '*',
+//     methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH', 'OPTIONS']
+// }));
 
 app.use('/', router);
 
