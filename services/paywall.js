3b9e35962d547d39cf8011d0c4aacd360a34bb44
taha <taha@dmdmax.com>
2021-11-03 16:00:49 +0500

    savePreference + activeUserLogs opened

--- services/paywall.js
+++ services/paywall.js
@@ -707,9 +707,9 @@ exports.updateFcmToken = async (req,res) => {
 
 exports.savePreference = async (req,res) => {
 	try{
-		// const body = req.body;
-		// let { data } = await axios.post(`${config.microservices.user_or_otp_service}/preference/save`, body);
-		let data = {code: 0, message: "Record inserted successfully!"};
+		const body = req.body;
+		let { data } = await axios.post(`${config.microservices.user_or_otp_service}/preference/save`, body);
+		// let data = {code: 0, message: "Record inserted successfully!"};
 		res.send(data);
 	}
 	catch(err){
@@ -732,9 +732,9 @@ exports.updateNotificationOpenedCount = async (req,res) => {
 
 exports.activeUserLogs = async (req,res) => {
 	try{
-		// const body = req.body;
-		// let { data } = await axios.post(`${config.microservices.user_or_otp_service}/active-user-log`, body);
-		let data = {code: 0, message: "Record inserted successfully!"};
+		const body = req.body;
+		let { data } = await axios.post(`${config.microservices.user_or_otp_service}/active-user-log`, body);
+		// let data = {code: 0, message: "Record inserted successfully!"};
 		res.send(data);
 	}
 	catch(err){
